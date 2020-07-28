54fea965da5b7c49118e64292e445caaf2ec29ad
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-07-28 11:18:53 -0500

    Clase 9

--- router.js
+++ router.js
@@ -4,10 +4,24 @@ class Router {
     this.routes = routes;
     this._loadInitialRoute();
   }
+
+  _matchUrlToRoute(urlSegs){
+    const matchedRoute = this.routes.find(route => {
+      const routePathSegs = route.path.split('/').slice(1)
+      if(routePathSegs.length != urlSegs.length){
+        return false;
+      }
+
+      return routePathSegs.every((routePathSegs, i) => routePathSegs === urlSegs[i])
+    });
+    
+    return matchedRoute;
+
+  _loadInitialRoute(){
+    const pathNameSplit = window.location.pathname.split('/');
+    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
+    this.loadRoute(...pathSegs);
+  }
+
 }
 
-_loadInitialRoute(){
-  const pathNameSplit = window.location.pathname.split('/');
-  const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
-  this.loadRoute(...pathSegs);
-}
